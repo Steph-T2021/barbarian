433c14bc420a5b4e7abfbdc56a1887d3534b0483
Stephanie Temmerman <greytem@hotmail.com>
2021-03-24 16:18:42 -0700

    styling for barbarian

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import logo from './data/logo/logo.png';
+import arrow from "./data/logo/arrow.png";
 import './App.css';
 
 function App() {
@@ -6,18 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+       
+   
+  
+    <img src={arrow} className="App-arrow" alt="arrrow"/>
       </header>
+        <p className="App-info">
+    
+        Resume/Portfolio coming soon!
+        </p>
     </div>
   );
 }
